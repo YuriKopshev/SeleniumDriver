77d7bd8ce10941ba1d660a31935143dfc81959d6
Yuri Kopshev <yura.kopshev@gmail.com>
2020-09-14 14:23:51 +0200

    changed chromedriver on linux

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -22,7 +22,7 @@ public class CardOrderTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver");
 
     }
 
